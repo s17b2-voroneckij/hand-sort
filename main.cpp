f0078a64d2dbda6f18ced551e24934fbcb1278a9
s17b2-voroneckij <34840323+s17b2-voroneckij@users.noreply.github.com>
2018-06-13 19:35:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using std::set;
 using std::pair;
 using std::cout;
 using std::endl;
+using std::make_pair;
 
 struct edge {
     int flow = 0;
@@ -16,39 +17,39 @@ struct edge {
 };
 
 class flow {
-    vector<edge> our;
-    vector<vector<int>> g;
-    vector<int> p;
-    vector<int> d;
+    vector<edge> edges;
+    vector<vector<int>> graph;
+    vector<int> potential;
+    vector<int> dist;
     vector<int> Prev;
-    int n;
+    size_t n;
     long long cost = 0;
 
     void ford_bellman() {
-        p.assign(n + 1, 1e9);
-        p[1] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < our.size(); j++) {
-                auto e = our[j];
-                if (p[e.v] < 1e9 && p[e.u] > p[e.v] + e.t)
-                    p[e.u] = p[e.v] + e.t, Prev[e.u] = j;
-                if (p[e.u] < 1e9 && p[e.v] > p[e.u] + e.t)
-                    p[e.v] = p[e.u] + e.t, Prev[e.v] = j;
+        potential.assign(n + 1, 1e9);
+        potential[1] = 0;
+        for (size_t i = 0; i < n - 1; i++) {
+            for (size_t j = 0; j < edges.size(); j++) {
+                auto e = edges[j];
+                if (potential[e.v] < 1e9 && potential[e.u] > potential[e.v] + e.t)
+                    potential[e.u] = potential[e.v] + e.t, Prev[e.u] = j;
+                if (potential[e.u] < 1e9 && potential[e.v] > potential[e.u] + e.t)
+                    potential[e.v] = potential[e.u] + e.t, Prev[e.v] = j;
             }
         }
     }
 
     void dijkstra() {
-        d.assign(n + 1, 1e9);
-        d[1] = 0;
+        dist.assign(n + 1, 1e9);
+        dist[1] = 0;
         set<pair<int, int>> heap;
-        for (int i = 1; i <= n; i++)
-            heap.insert({d[i], i});
+        for (size_t i = 1; i <= n; i++)
+            heap.insert({dist[i], i});
         while (!heap.empty()) {
             int curr = heap.begin()->second;
             heap.erase(heap.begin());
-            for (auto num : g[curr]) {
-                auto edge = our[num];
+            for (auto num : graph[curr]) {
+                auto edge = edges[num];
                 int w = edge.t;
                 int oth = curr ^ edge.v ^ edge.u;
 
@@ -56,55 +57,56 @@ class flow {
                     continue;
                 if (edge.flow != 0)
                     w *= -1;
-                w = w + p[curr] - p[oth];
-                if (d[oth] > d[curr] + w) {
-                    heap.erase({d[oth], oth});
-                    d[oth] = d[curr] + w;
+                w = w + potential[curr] - potential[oth];
+                if (dist[oth] > dist[curr] + w) {
+                    heap.erase({dist[oth], oth});
+                    dist[oth] = dist[curr] + w;
                     Prev[oth] = num;
-                    heap.insert({d[oth], oth});
+                    heap.insert({dist[oth], oth});
                 }
             }
         }
 
-        for (int i = 1; i <= n; i++)
-            p[i] += d[i];
+        for (size_t i = 1; i <= n; i++)
+            potential[i] += dist[i];
     }
 
 public:
     void set_n(int a) {
         n = a;
-        g.resize(n + 1);
-        d.resize(n + 1);
+        graph.resize(n + 1);
+        dist.resize(n + 1);
         Prev.resize(n + 1);
-        p.resize(n + 1);
+        potential.resize(n + 1);
     }
 
     void add_edge(edge curr) {
-        g[curr.u].push_back(our.size());
-        g[curr.v].push_back(our.size());
-        our.push_back(curr);
+        graph[curr.u].push_back(edges.size());
+        graph[curr.v].push_back(edges.size());
+        edges.push_back(curr);
     }
 
     bool add_flows(int k) {
-        for (int i = 0; i < k; i++) {
-            if (p[n] > 9e8 || d[n] > 9e8) {
+        ford_bellman();
+        for (size_t i = 0; i < k; i++) {
+            if (potential[n] > 9e8 || dist[n] > 9e8) {
                 return false;
             }
 
             int curr = n;
             while (curr != 1) {
                 int road = Prev[curr];
-                if (abs(our[road].flow) == 1)
-                    our[road].flow = 0;
-                else if (our[road].u == curr)
-                    our[road].flow = 1;
+                if (abs(edges[road].flow) == 1)
+                    edges[road].flow = 0;
+                else if (edges[road].u == curr)
+                    edges[road].flow = 1;
                 else
-                    our[road].flow = -1;
-                if (our[road].flow == 0)
-                    cost -= our[road].t;
+                    edges[road].flow = -1;
+                if (edges[road].flow == 0)
+                    cost -= edges[road].t;
                 else
-                    cost += our[road].t;
-                curr = curr ^ our[road].v ^ our[road].u;
+                    cost += edges[road].t;
+                curr = curr ^ edges[road].v ^ edges[road].u;
 
             }
             dijkstra();
@@ -114,15 +116,15 @@ public:
 
     vector<vector<int>> get_ways(int k) {
         vector<vector<int>> res(k);
-        for (int i = 0; i < k; i++) {
+        for (size_t i = 0; i < k; i++) {
             int curr = 1;
             while (curr != n) {
-                for (auto num : g[curr]) {
-                    auto edge = our[num];
+                for (auto num : graph[curr]) {
+                    auto edge = edges[num];
                     if ((edge.v == curr && edge.flow == 1) || (edge.u == curr && edge.flow == -1)) {
                         res[i].push_back(num);
                         curr = edge.u ^ edge.v ^ curr;
-                        our[num].flow = 0;
+                        edges[num].flow = 0;
                         break;
                     }
                 }
@@ -135,37 +137,40 @@ public:
         return cost;
     }
 
-    void start() {
-        ford_bellman();
-    }
 };
 
-int main() {
+auto read_graph() {
     int n, m, k;
     cin >> n >> m >> k;
-    flow our;
-    our.set_n(n);
+    flow graph;
+    graph.set_n(n);
 
-    for (int i = 0; i < m; i++) {
+    for (size_t i = 0; i < m; i++) {
         edge temp;
         cin >> temp.v >> temp.u >> temp.t;
-        our.add_edge(temp);
+        graph.add_edge(temp);
     }
+    return make_pair(k, graph);
+}
 
-    our.start();
-
-    if (!our.add_flows(k)) {
+void solve(int k, flow graph) {
+    if (!graph.add_flows(k)) {
         cout << -1;
-        return 0;
+        return ;
     }
 
 
     cout.precision(20);
-    cout << (double)our.get_cost() / k << endl;
-    for (const auto& way : our.get_ways(k)) {
+    cout << (double)graph.get_cost() / k << endl;
+    for (const auto& way : graph.get_ways(k)) {
         cout << way.size() << " ";
         for (int v : way)
             cout << v + 1 << " ";
         cout << "\n";
     }
+}
+
+int main() {
+    auto input = read_graph();
+    solve(input.first, input.second);
 }
